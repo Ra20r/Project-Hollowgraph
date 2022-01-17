1057b0869ba07af69afd6ca36ce0704f9063d121
Ivan-Denisovich-py <bhaswarishi2908@gmail.com>
2022-01-18 02:17:36 +0530

    Added a renderer

--- render/index.js
+++ render/index.js
@@ -1,15 +1,14 @@
-// fetch("http://127.0.0.1:5500/Data/first_test_data.csv")
-//     .then((response) =>
-//         // console.log(response)
-//     )
-//     .catch((err) => console.log(err))
-
 p5.disableFriendlyErrors = true
-const url = "http://127.0.0.1:5500/Data/first_test_data.csv"
+const url = "http://127.0.0.1:5500/Data/data.csv"
 let data
+let scl = 10
+let spaceBetween = 100
+let zoff = 0
+let yoff = 30
+let xoff = 0
 
-let angle = 0
-const PHI = (Math.sqrt(5) + 1) / 2
+let alpha = 0
+let beta = 0
 
 function preload() {
     Papa.parse(url, {
@@ -33,20 +32,47 @@ function setup() {
 
 function draw() {
     if (!data) return
-    console.log(data)
-    noLoop()
 
     background(0)
     ambientLight(200)
     directionalLight(255, 255, 255, 0, 0, 1)
     normalMaterial()
-    // rotateX(angle)
-    // rotateY(angle * PHI)
+    rotateY(alpha)
+    rotateX(beta)
 
     fill(255)
-    beginShape()
+    stroke(0)
+    strokeWeight(0.5)
 
-    endShape(CLOSE)
+    for (let i = 0; i < data[0].length - 1; i++) {
+        beginShape(TRIANGLE_STRIP)
+        for (let j = 0; j < data.length - 1; j++) {
+            let a = data[j][i] * scl
+            let b = data[j][i + 1] * scl
+            let theta = -radians(2 * j)
+            let x = a * cos(theta) + xoff
+            let y = a * sin(theta) + yoff
+            let z = -i * spaceBetween - zoff
+            vertex(x, y, z)
+            x = b * cos(theta) + xoff
+            y = b * sin(theta) + yoff
+            vertex(x, y, z - spaceBetween)
+        }
+        endShape()
+    }
 
-    // angle += 0.01
+    checkControls()
+}
+
+function checkControls() {
+    if (keyIsDown(87)) zoff -= 10 // W
+    if (keyIsDown(83)) zoff += 10 // S
+    if (keyIsDown(65)) xoff += 10 // A
+    if (keyIsDown(68)) xoff -= 10 // D
+    if (keyIsDown(32)) yoff += 1 // space
+    if (keyIsDown(16)) yoff -= 1 // left shift
+    if (keyIsDown(LEFT_ARROW)) alpha += degrees(0.0005)
+    if (keyIsDown(RIGHT_ARROW)) alpha -= degrees(0.0005)
+    if (keyIsDown(UP_ARROW)) beta -= degrees(0.0005)
+    if (keyIsDown(DOWN_ARROW)) beta += degrees(0.0005)
 }
